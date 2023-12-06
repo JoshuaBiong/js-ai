f4dd8bf9a9718ca044bbeec08cb5d3a02f7d7dd2
Joshua Biong <joshuabiong7@gmail.com>
2023-12-06 22:10:58 +0800

    adding server side

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenApi } from 'openai'
+
+
+
+dotenv.config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenApi(configuration)
